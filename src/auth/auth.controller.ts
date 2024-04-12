cc4ffdcc89ec2ceb50bd21a9a6c4679a6021f8bb
AsetCoder <aset16006@gmail.com>
2024-04-12 22:53:00 +0500

    Cotroller is pulled

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { signupDto } from './dto/signup.dto';
+import { siginDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  signup(@Body() data: signupDto) {
+    return this.authService.signup(data)
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  signin(@Body() data: siginDto) {
+    return this.authService.signin(data)
+  }
 }
